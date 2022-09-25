042f636feed2a907df19060a6f511f534c9442dd
simonemanfre <simone.manfredini@trapella.it>
2022-09-25 19:24:35 +0200

    feat: esercizio1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 
+import Esercizio1 from "./components/organisms/Esercizio1/Esercizio1";
 import Esercizio2 from "./components/organisms/Esercizio2/Esercizio2";
 import Esercizio4 from "./components/organisms/Esercizio4/Esercizio4";
 
 function App() {
   return (
     <>
+      <Esercizio1 />
       <Esercizio2 />
       <Esercizio4 />
     </>
---
+++ src/components/organisms/Esercizio1/Esercizio1.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./esercizio1.scss";
+
+import Container from "../../atoms/Container/Container";
+import Title from "../../atoms/title/Title";
+import Button from "../../atoms/button/Button";
+
+const Esercizio1 = (props) => {
+  return (
+    <header className="c-hero">
+      <Container className="c-hero__container">
+        <Title tag="h1" size="1" className="c-hero__title">
+          Lorem ipsum dolor sit amet.
+        </Title>
+
+        <section className="c-hero__text">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+            eaque repellat quae voluptatibus dignissimos accusamus dolor nisi
+            iure facilis doloribus!
+          </p>
+        </section>
+
+        <Button url="#" className="c-hero__button" text="Approfondisci" />
+      </Container>
+    </header>
+  );
+};
+
+export default Esercizio1;
