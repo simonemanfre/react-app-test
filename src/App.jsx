2d760bdcd50408fb60ed9674a6ec3531ffdf4995
simonemanfre <simone.manfredini@trapella.it>
2022-09-25 18:54:53 +0200

    feat: esercizio2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 
+import Esercizio2 from "./components/organisms/Esercizio2/Esercizio2";
 import Esercizio4 from "./components/organisms/Esercizio4/Esercizio4";
 
 function App() {
   return (
     <>
+      <Esercizio2 />
       <Esercizio4 />
     </>
   );
